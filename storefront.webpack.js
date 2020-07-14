4813293c38ed3ae98a8229bb324c39814e773652
Leonardo Matos <leomp120894@gmail.com>
2020-07-14 19:46:52 -0300

    Create storefront.webpack.js

---
+++ storefront.webpack.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const dirSearchAlias = path.resolve(__dirname, 'template/js/lib/search-engine')
+const pathDslAlias = path.resolve(dirSearchAlias, 'dsl')
+
+module.exports = () => {
+  resolve: {
+    alias: {
+      './lib/dsl': pathDslAlias,
+      './../lib/dsl': pathDslAlias,
+      '../lib/dsl': pathDslAlias,
+      './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term')
+    }
+  }
+}
